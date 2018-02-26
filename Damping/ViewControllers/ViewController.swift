f481de4b36efffb76902af0bfa88c1ea3536d697
undefined <undefined@pisem.net>
2018-02-26 21:44:46 +0300

    Details are on the form now.

--- Damping/ViewControllers/ViewController.swift
+++ Damping/ViewControllers/ViewController.swift
@@ -16,6 +16,16 @@ class ViewController: NSViewController {
                 alert.addButton(withTitle: "OK")
                 alert.runModal()
                 
+                T.objectValue = Shared.instance.T
+                K.objectValue = Shared.instance.K
+                M.objectValue = Shared.instance.M
+                N.objectValue = Shared.instance.N
+                R.objectValue = Shared.instance.R
+                h_r.objectValue = Shared.instance.h_r
+                h_phi.objectValue = Shared.instance.h_phi
+                tau.objectValue = Shared.instance.tau
+                Position.objectValue = Double(Shared.instance.actuatorIndex) / Double(Shared.instance.M)
+                Accuracy.objectValue = Shared.instance.accuracy
             } catch {
                 let alert = NSAlert()
                 alert.messageText = "Ошибка чтения файла"
@@ -26,6 +36,17 @@ class ViewController: NSViewController {
             }
         }
     }
+    
+    @IBOutlet var T: NSTextField!
+    @IBOutlet var K: NSTextField!
+    @IBOutlet var M: NSTextField!
+    @IBOutlet var N: NSTextField!
+    @IBOutlet var R: NSTextField!
+    @IBOutlet var h_r: NSTextField!
+    @IBOutlet var h_phi: NSTextField!
+    @IBOutlet var tau: NSTextField!
+    @IBOutlet var Position: NSTextField!
+    @IBOutlet var Accuracy: NSTextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
